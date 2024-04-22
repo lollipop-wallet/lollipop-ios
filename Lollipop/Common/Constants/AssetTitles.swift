07ff0ea64af85ef6330383e0f2be468aca4d55cd
Aleksandar Draskovic <alexandremne@gmail.com>
2024-04-22 14:53:08 +0200

    Add new icons and app strings.

--- Lollipop/Common/Constants/AssetTitles.swift
+++ Lollipop/Common/Constants/AssetTitles.swift
@@ -10,4 +10,8 @@ import Foundation
 struct AssetTitles {
     static let splashIcon = "ic_splash_lollipop"
     static let backIcon = "ic_back"
+    static let onboardingIcon1 = "ic_onboarding_1"
+    static let onboardingIcon2 = "ic_onboarding_2"
+    static let onboardingIcon3 = "ic_onboarding_3"
+    static let onboardingIcon4 = "ic_onboarding_4"
 }
--- Lollipop/Resources/Localization/LocalizedTitle.swift
+++ Lollipop/Resources/Localization/LocalizedTitle.swift
@@ -12,6 +12,7 @@ import Localize_Swift
 enum LocalizedTitle: String {
     case cancel
     case openSettings
+    
 
     var localized: String {
         return self.rawValue.localized()
