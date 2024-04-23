f30a8022dbf11aef55e995497208a53625882048
Aleksandar Draskovic <alexandremne@gmail.com>
2024-04-23 11:25:58 +0200

    Start working on SignIn page.

--- Lollipop/App Flow/Registration/Sign in/UI/SignInUI.swift
+++ Lollipop/App Flow/Registration/Sign in/UI/SignInUI.swift
@@ -10,5 +10,17 @@ import UIKit
 extension SignInView {
     func setup() {
         SignInWireframe.createModule(SignInRef: self)
+        
+        lazy var backgroundImage: UIImageView = {
+            let imageView = UIImageView()
+            imageView.image = UIImage(named: AssetTitles.signInBackgroundIcon)
+            imageView.contentMode = .scaleAspectFill
+            return imageView
+        }()
+        
+        self.view.addSubview(backgroundImage)
+        backgroundImage.snp.makeConstraints { make in
+            make.leading.trailing.top.bottom.equalToSuperview()
+        }
     }
 }
