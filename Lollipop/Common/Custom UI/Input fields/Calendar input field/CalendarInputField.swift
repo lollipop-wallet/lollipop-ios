aa20d602eb5d387b2b6624c3cc84cb43002206da
Aleksandar Draskovic <alexandremne@gmail.com>
2024-04-24 10:33:56 +0200

    Add outlets to calendar control.

--- Lollipop/Common/Custom UI/Input fields/Calendar input field/CalendarInputField.swift
+++ Lollipop/Common/Custom UI/Input fields/Calendar input field/CalendarInputField.swift
@@ -9,6 +9,41 @@ import UIKit
 
 class CalendarInputField: UIView {
     
+    //MARK: Outlets
+    
+    lazy var inputLabel: UILabel = {
+        let label = UILabel()
+        label.font = .inter(ofSize: 14, name: .light)
+        label.textColor = AppColors.darkGrey
+        label.textAlignment = .left
+        label.snp.makeConstraints { make in
+            make.height.equalTo(20)
+        }
+        return label
+    }()
+    
+    lazy var inputTitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .inter(ofSize: 14, name: .light)
+        label.textColor = AppColors.black
+        label.textAlignment = .left
+        label.snp.makeConstraints { make in
+            make.height.equalTo(20)
+        }
+        return label
+    }()
+    
+    lazy var inputErrorLabel: UILabel = {
+        let label = UILabel()
+        label.font = .inter(ofSize: 10, name: .light)
+        label.textColor = AppColors.error
+        label.textAlignment = .left
+        label.snp.makeConstraints { make in
+            make.height.equalTo(12)
+        }
+        return label
+    }()
+    
     required init() {
         super.init(frame: .zero)
         setup()
