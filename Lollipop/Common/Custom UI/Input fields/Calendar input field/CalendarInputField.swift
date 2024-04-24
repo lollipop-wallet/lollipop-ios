ddcdc4275bbaa607a71dc9f671439427aa93feb5
Aleksandar Draskovic <alexandremne@gmail.com>
2024-04-24 10:18:39 +0200

    Setting up calendar input view.

---
+++ Lollipop/Common/Custom UI/Input fields/Calendar input field/CalendarInputField.swift
@@ -0,0 +1,27 @@
+//
+//  CalendarInputField.swift
+//  Lollipop
+//
+//  Created by Aleksandar Draskovic on 24.4.24..
+//
+
+import UIKit
+
+class CalendarInputField: UIView {
+    
+    required init() {
+        super.init(frame: .zero)
+        setup()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+    }
+    
+    
+    func setup() {
+    }
+}
