b1379fe1b904dc0703d034f1deedd374c9db8373
Aleksandar Draskovic <alexandremne@gmail.com>
2024-05-04 08:01:42 +0200

    Navigation bar changes for promotion details.

--- Lollipop/App Flow/Main/Tabs/Home/Promotions/Promotion details/PromotionDetailsView.swift
+++ Lollipop/App Flow/Main/Tabs/Home/Promotions/Promotion details/PromotionDetailsView.swift
@@ -12,10 +12,25 @@ import UIKit
 class PromotionDetailsView: UIViewController, PromotionDetailsViewProtocol {
 
     var presenter: PromotionDetailsPresenterProtocol?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+    }
 
 	override func viewDidLoad() {
         super.viewDidLoad()
         setup()
+    }
+    
+    
+    //MARK: Actions
+    @objc func onBackTap() {
+        popBack(2)
     }
 
 }
--- Lollipop/App Flow/Main/Tabs/Home/Promotions/Promotion details/UI/PromotionDetailsUI.swift
+++ Lollipop/App Flow/Main/Tabs/Home/Promotions/Promotion details/UI/PromotionDetailsUI.swift
@@ -11,5 +11,16 @@ extension PromotionDetailsView {
     func setup() {
         self.view.backgroundColor = AppColors.lightGrey
         PromotionDetailsWireframe.createModule(PromotionDetailsRef: self)
+        
+        lazy var backButton: UIBarButtonItem = {
+            let button = UIBarButtonItem()
+            button.image = UIImage(named: AssetTitles.backIcon)
+            button.tintColor = AppColors.black
+            button.action = #selector(self.onBackTap)
+            button.target = self
+            return button
+        }()
+        
+        self.navigationItem.leftBarButtonItems = [backButton]
     }
 }
