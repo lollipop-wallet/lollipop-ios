7bda7ea412bb77b161b14b29d6623e28a19a590e
Aleksandar Draskovic <alexandremne@gmail.com>
2024-05-04 14:43:57 +0200

    Code cleanup.

--- Lollipop/App Flow/Main/Tabs/Home/Wallet/Wallet card/WalletCardPresenter.swift
+++ Lollipop/App Flow/Main/Tabs/Home/Wallet/Wallet card/WalletCardPresenter.swift
@@ -7,7 +7,7 @@
 //
 import UIKit
 
-class WalletCardPresenter:WalletCardPresenterProtocol  {
+class WalletCardPresenter: NSObject, WalletCardPresenterProtocol  {
     
     var interactor : WalletCardInputInteractorProtocol?
     weak var view: WalletCardViewProtocol?
@@ -19,3 +19,19 @@ extension WalletCardPresenter: WalletCardOutputInteractorProtocol {
     
 }
 
+//MARK: UITableViewDelegate&Datasource
+extension WalletCardPresenter {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CellId.walletCardShopCell.rawValue, for: indexPath) as! WalletCardShopTableViewCell
+        cell.configureWith(index: indexPath, delegate: self)
+        return cell
+    }
+    
+    func didSelectItemAt(index: IndexPath) {
+        
+    }
+}
---
+++ Lollipop/Common/Custom UI/Custom app components/Wallet stack/WalletStack.swift
@@ -0,0 +1,26 @@
+//
+//  WalletStack.swift
+//  Lollipop
+//
+//  Created by Aleksandar Draskovic on 4.5.24..
+//
+
+import UIKit
+
+class WalletStack: UIView {
+    
+    required init() {
+        super.init(frame: .zero)
+        setup()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+    }
+    
+    func setup() {
+    }
+}
