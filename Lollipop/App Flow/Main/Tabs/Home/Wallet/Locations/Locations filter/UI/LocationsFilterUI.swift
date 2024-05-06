c5b2ac107d92bfb5a6da23e61ab726f590d76c05
Aleksandar Draskovic <alexandremne@gmail.com>
2024-05-06 06:28:26 +0200

    Add filter top pull bar.

--- Lollipop/App Flow/Main/Tabs/Home/Wallet/Locations/Locations filter/UI/LocationsFilterUI.swift
+++ Lollipop/App Flow/Main/Tabs/Home/Wallet/Locations/Locations filter/UI/LocationsFilterUI.swift
@@ -11,5 +11,21 @@ extension LocationsFilterView {
     func setup() {
         self.view.backgroundColor = AppColors.white
         LocationsFilterWireframe.createModule(LocationsFilterRef: self)
+        
+        lazy var upperPullBar: UIView = {
+            let view = UIView()
+            view.layer.cornerRadius = 2
+            view.layer.masksToBounds = true
+            view.backgroundColor = AppColors.darkGrey
+            return view
+        }()
+        
+        self.view.addSubview(upperPullBar)
+        upperPullBar.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(8)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(40)
+            make.height.equalTo(4)
+        }
     }
 }
