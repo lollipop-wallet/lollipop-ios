fa7b43d80115dad71c33ec6b52c4322b08a76f55
Aleksandar Draskovic <alexandremne@gmail.com>
2024-05-25 06:54:42 +0200

    Add home item type.

---
+++ Lollipop/Common/Enumerations/HomeItemType.swift
@@ -0,0 +1,16 @@
+//
+//  HomeItemType.swift
+//  Lollipop
+//
+//  Created by Aleksandar Draskovic on 25/05/2024.
+//
+
+import Foundation
+
+enum HomeItemType {
+    case addcard
+    case cards
+    case brand
+    case poster
+    case promotion
+}
