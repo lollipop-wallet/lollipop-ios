4b24df928c08eaf955c38b90f1cd009e51929582
Aleksandar Draskovic <alexandremne@gmail.com>
2024-05-31 10:18:23 +0200

    Add enum for code type.

---
+++ Lollipop/Common/Enumerations/CardCodeType.swift
@@ -0,0 +1,14 @@
+//
+//  CardCodeType.swift
+//  Lollipop
+//
+//  Created by Aleksandar Draskovic on 31/05/2024.
+//
+
+import Foundation
+
+enum CardCodeType: String {
+    case barcode = "bar_code"
+    case qrcode = "qr_code"
+    case plaincode = "plain_code"
+}
