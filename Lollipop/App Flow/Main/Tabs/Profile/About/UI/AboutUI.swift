2a3f758ad28ff985a07ecd66897c8b1beceb2181
Aleksandar Draskovic <alexandremne@gmail.com>
2024-07-14 10:51:50 +0200

    Finish About.

--- Lollipop/App Flow/Main/Tabs/Profile/About/UI/AboutUI.swift
+++ Lollipop/App Flow/Main/Tabs/Profile/About/UI/AboutUI.swift
@@ -14,10 +14,14 @@ extension AboutView {
         
         let appearance = UINavigationBarAppearance()
         appearance.largeTitleTextAttributes = [.foregroundColor: AppColors.black, .font : UIFont.inter(ofSize: 30, name: .bold)]
+        appearance.titleTextAttributes = [.foregroundColor: AppColors.black, .font : UIFont.inter(ofSize: 20, name: .bold)]
         let imgBackArrow = UIImage(named: AssetTitles.backIcon)?.withAlignmentRectInsets(UIEdgeInsets(top: 0, left: 0, bottom: 1.5, right: 0))
         appearance.setBackIndicatorImage(imgBackArrow, transitionMaskImage: imgBackArrow)
+        appearance.backgroundColor = AppColors.lightGrey
         navigationItem.standardAppearance = appearance
+        navigationItem.scrollEdgeAppearance = appearance
         UINavigationBar.appearance().layoutMargins.left = 20
+        
         self.navigationItem.title = LocalizedTitle.aboutApp.localized
         
         lazy var backButton: UIBarButtonItem = {
@@ -88,6 +92,56 @@ extension AboutView {
             return imageView
         }()
         
+        lazy var secondTitleLabel: UILabel = {
+            let label = UILabel()
+            label.font = .inter(ofSize: 18, name: .semibold)
+            label.textAlignment = .left
+            label.textColor = AppColors.black
+            label.numberOfLines = 0
+            label.lineBreakMode = .byWordWrapping
+            label.text = LocalizedTitle.aboutTitle2.localized
+            return label
+        }()
+        
+        lazy var secondSubtitleLabel: UILabel = {
+            let label = UILabel()
+            label.font = .inter(ofSize: 16, name: .regular)
+            label.textAlignment = .left
+            label.textColor = AppColors.black
+            label.numberOfLines = 0
+            label.lineBreakMode = .byWordWrapping
+            label.text = LocalizedTitle.aboutSubtitle2.localized
+            return label
+        }()
+        
+        lazy var thirdTitleLabel: UILabel = {
+            let label = UILabel()
+            label.font = .inter(ofSize: 18, name: .semibold)
+            label.textAlignment = .left
+            label.textColor = AppColors.black
+            label.numberOfLines = 0
+            label.lineBreakMode = .byWordWrapping
+            label.text = LocalizedTitle.aboutTitle3.localized
+            return label
+        }()
+        
+        lazy var thirdSubtitleLabel: UILabel = {
+            let label = UILabel()
+            label.font = .inter(ofSize: 16, name: .regular)
+            label.textAlignment = .left
+            label.textColor = AppColors.black
+            label.numberOfLines = 0
+            label.lineBreakMode = .byWordWrapping
+            label.text = LocalizedTitle.aboutSubtitle3.localized
+            return label
+        }()
+        
+        lazy var imageThree: UIImageView = {
+            let imageView = UIImageView()
+            imageView.image = UIImage(named: AssetTitles.aboutIcon3)
+            imageView.contentMode = .scaleAspectFill
+            return imageView
+        }()
         
         self.view.addSubview(scrollView)
         scrollView.snp.makeConstraints { make in
@@ -137,6 +191,43 @@ extension AboutView {
             make.leading.equalToSuperview().offset(20)
             make.trailing.equalToSuperview().offset(-20)
             make.height.equalTo((self.view.frame.width - 40) * 0.71)
+        }
+        
+        contentView.addSubview(secondTitleLabel)
+        secondTitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(imageTwo.snp.bottom).offset(32)
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.equalToSuperview().offset(-20)
+        }
+        
+        contentView.addSubview(secondSubtitleLabel)
+        secondSubtitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(secondTitleLabel.snp.bottom).offset(16)
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.equalToSuperview().offset(-20)
+        }
+        
+        contentView.addSubview(thirdTitleLabel)
+        thirdTitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(secondSubtitleLabel.snp.bottom).offset(32)
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.equalToSuperview().offset(-20)
+        }
+        
+        contentView.addSubview(thirdSubtitleLabel)
+        thirdSubtitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(thirdTitleLabel.snp.bottom).offset(16)
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.equalToSuperview().offset(-20)
+        }
+        
+        contentView.addSubview(imageThree)
+        imageThree.snp.makeConstraints { make in
+            make.top.equalTo(thirdSubtitleLabel.snp.bottom).offset(32)
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.equalToSuperview().offset(-20)
+            make.height.equalTo((self.view.frame.width - 40) * 0.71)
+            make.bottom.equalToSuperview().offset(-20)
         }
     }
 }
